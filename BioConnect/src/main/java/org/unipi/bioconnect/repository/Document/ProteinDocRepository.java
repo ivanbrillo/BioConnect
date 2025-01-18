a691b5c43153504bc845ecf9c91366617a4fb362
Andrea Bochicchio <a.bochicchio.inbox@gmail.com>
2025-01-19 00:38:36 +0100

    regex to text

--- BioConnect/src/main/java/org/unipi/bioconnect/repository/Document/ProteinDocRepository.java
+++ BioConnect/src/main/java/org/unipi/bioconnect/repository/Document/ProteinDocRepository.java
@@ -10,8 +10,9 @@ import java.util.List;
 
 public interface ProteinDocRepository extends MongoRepository<ProteinDoc, String> {
 
-    @Query(value = "{ '$or': [ { '_id': ?0 }, { 'name': { '$regex': ?0, '$options': 'i' } } ] }", fields = "{ '_class': 0 }")  // projection excluding class attribute (automatically added by mongo drivers)
+    @Query(value = "{ '$or': [ { '_id': ?0 }, { '$text': { '$search': '\\\"?0\\\"' } } ] }", fields = "{ '_class': 0 }")  // projection excluding class attribute (automatically added by mongo drivers)
     List<ProteinDocDTO> findByIdOrNameContainingIgnoreCase(String searchedText);
+
 
     @Query(value = "{ 'pathways': ?0, 'subcellularLocations': ?1 }", fields = "{ '_class': 0 }")
     List<ProteinDocDTO> findByPathwayAndSubcellularLocation(String pathway, String subcellularLocation);
